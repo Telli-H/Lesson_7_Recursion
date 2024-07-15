aff0e6f015e836df880ff8ab05a35a12a61c9614
Telli <tellihamzayeva@gmail.com>
2024-07-16 02:47:11 +0400

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int[] arr={1,2,3,4,5};
+
+PrintArrReversed(arr, arr.Length-1);
+
+static void PrintArrReversed(int[] arr, int index)
+{
+    if(index<0)
+    {
+       return;  
+    }
+   
+
+    Console.WriteLine(arr[index]);
+    PrintArrReversed(arr, index-1);
+}
